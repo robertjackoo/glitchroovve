08df93daf3c39b5d91e80afbcaee1764c960aad0
robertjackoo <131236014+robertjackoo@users.noreply.github.com>
2023-11-29 22:28:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const UUID = process.env.UUID || 'c1cccba1-bb03-4e4a-9480-260e7ad4eff0';
+const UUID = process.env.UUID || 'de04add9-5c68-8bab-870c-08cd5320df00';
 const projectPageURL = process.env.URL || 'https://profuse-charmed-clover.glitch.me/';// 填写项目域名可开启自动访问保活,例如：https://www.google.com
 const intervalInMilliseconds = process.env.TIME || 2 * 60 * 1000;  // 自动访问间隔时间（2分钟）
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.biudpam.eu.org';    // 哪吒3个变量不全不运行
